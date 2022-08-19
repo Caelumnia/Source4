69af2f5a98a5d98395e7a93e27a5382d332f7bcf
Caelumnia <caelumnia@outlook.com>
2022-08-19 15:12:45 +0800

    cleanup

--- Plugins/AutoMesh/Source/AutoMesh/Public/Stats.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-DECLARE_STATS_GROUP(TEXT("AutoMesh"), STATGROUP_AutoMesh, STATCAT_Advanced)
-DECLARE_DWORD_COUNTER_STAT(TEXT("AutoMesh Triangles"), STAT_AutoMesh_Triangles, STATGROUP_AutoMesh)
-DECLARE_DWORD_COUNTER_STAT(TEXT("AutoMesh Batches"), STAT_AutoMesh_Batches, STATGROUP_AutoMesh)
-DECLARE_CYCLE_STAT(TEXT("DrawStaticMesh"), STAT_AutoMesh_DrawStaticMesh, STATGROUP_AutoMesh)
-DECLARE_CYCLE_STAT(TEXT("GetDynamicMesh"), STAT_AutoMesh_GetDynamicMesh, STATGROUP_AutoMesh)
-DECLARE_CYCLE_STAT(TEXT("GetMeshBatches"), STAT_AutoMesh_GetMeshBatches, STATGROUP_AutoMesh)
